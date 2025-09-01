6a0441a8791102ea3f02bf44c91de96c65428366
Navista Andara Putri <navistaandaraputri@gmail.com>
2025-09-01 21:39:02 +0700

    Add files via upload

---
+++ Vista_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vista_project1;
+
+/**
+ *
+ * @author hp_5c
+ */
+public class Vista_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama    : Navista Andara Putri");
+        System.out.println("NPM     : 2440506059");
+        System.out.println("Alamat  : Mertoyudan");
+        System.out.println("No_HP   : 08123456789");
+    }
+}
