1a23d157d2ba98a12a633c45ae5f00ac47795bbf
Navista Andara Putri <navistaandaraputri@gmail.com>
2025-11-04 14:10:52 +0700

    Add files via upload

---
+++ PraktikumPBO_9/src/main/java/praktikum9/Mobil.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum9;
+
+/**
+ *
+ * @author hp_5c
+ */
+class Mobil extends Kendaraan {
+    @Override
+    void berjalan() {
+        System.out.println("Mobil sedang berjalan dengan cepat.");
+    }
+}
+
+
---
+++ PraktikumPBO_9_Tugas/src/main/java/praktikum9tugas/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praktikum9tugas;
+
+/**
+ *
+ * @author hp_5c
+ */
+public class Main {
+    public static void main(String[] args) {
+        Hewan kucing = new Kucing(); 
+        Hewan anjing = new Anjing();
+
+        kucing.suara();
+        anjing.suara();
+    }
+}
